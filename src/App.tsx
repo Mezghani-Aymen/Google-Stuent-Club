69c8206d3c2b26c2c214bf988853d08a7a39c911
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-17 14:35:58 +0100

    [fix] update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import { generateCircles } from './utils/circle';
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = useCallback(() => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      generateCircles(canvasRef.current);
+    }
+  }, [windowSize]);
+
   return (
-    <> </>
+    <div className=" w-screen h-screen text-white">
+      <canvas ref={canvasRef} className="w-screen h-screen bg-black fixed -z-10"></canvas>
+      <div className=" w-full h-full backdrop-blur-sm  fixed -z-5"></div>
+    </div>
+
   );
 }
 
