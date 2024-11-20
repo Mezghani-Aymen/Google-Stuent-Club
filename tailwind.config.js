c75cae98eabea5932c6670fbb5f6c21e40ffa7cc
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-20 19:16:05 +0100

    [fix] create custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-red': {
+          DEFAULT: '#DB4437',
+        },
+        'custom-green': {
+          DEFAULT: '#0F9D58',
+        },
+        'custom-blue': {
+          DEFAULT: '#4285F4',
+        },
+        'custom-yellow': {
+          DEFAULT: '#F4B400',
+        },
+      },
+    },
   },
   plugins: [],
 };
