12f337142db1c68a585dbf5869e5feac8d42a4fb
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-20 21:51:23 +0100

    [fix] call Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState, useCallback } from 'react';
 import { generateCircles } from './utils/circle';
+import Header from "./components/header";
+import Hero from "./components/hero/hero";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -29,12 +31,25 @@ function App() {
     }
   }, [windowSize]);
 
-  return (
-    <div className=" w-screen h-screen text-white">
-      <canvas ref={canvasRef} className="w-screen h-screen bg-black fixed -z-10"></canvas>
-      <div className=" w-full h-full backdrop-blur-sm  fixed -z-5"></div>
-    </div>
+  const navLink = [
+    { label: 'Home', href: 'HOME' },
+    { label: 'Events', href: '#' },
+    { label: 'Teams', href: '#' },
+    { label: 'Glory', href: '#' },
+    { label: 'Contact', href: '#CONTACT' },
+  ]
 
+  return (
+    <div className="text-white">
+      <canvas ref={canvasRef} className="w-screen h-screen bg-black fixed -z-10"></canvas>
+      <div className="w-full h-full  fixed -z-5 bg-white backdrop-filter backdrop-blur-sm bg-opacity-10 "></div>
+      <div>
+        <Header
+          navLinks={navLink}
+          signupButton={{ label: 'Glory', href: '#' }}
+        />
+      </div>
+    </div>
   );
 }
 
