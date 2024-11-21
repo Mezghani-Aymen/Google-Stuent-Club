e4fe0895cbc265cbbd6d5ad633e5edf00c1e356d
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-21 21:38:52 +0100

    [fix] call/use AnimationProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AnimationProvider } from './contexts/animationContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AnimationProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AnimationProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
