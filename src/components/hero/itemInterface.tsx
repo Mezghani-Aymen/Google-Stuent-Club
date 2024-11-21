f85cb1dd0c4683f9bb085505ca3b142da790ee8b
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-21 21:37:28 +0100

    [add] create interface Item

---
+++ src/components/hero/itemInterface.tsx
@@ -0,0 +1,10 @@
+export interface ItemProps {
+    title: string;
+    description: string;
+    image: string;
+    button: {
+        label: string;
+        href: string;
+    };
+    bgColor: string
+}
