ed98d4c287e5f3e271c1c33e4cb84db632d7837e
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-21 14:02:05 +0100

    [fix] update type files (.txs -> .ts)

--- src/types/headerInterface.ts
+++ src/types/headerInterface.ts
@@ -1,7 +1,10 @@
+import { ReactNode } from "react";
+
 export interface HeaderProps {
     navLinks: { label: string; href: string }[];
     signupButton: {
         label: string;
         href: string;
     };
+    children?: ReactNode;
 }
