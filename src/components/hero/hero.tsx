579f9f678265730981158af717815246d799cf63
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-11-24 15:43:43 +0100

    [fix] activate silde cards

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,101 +1,133 @@
-import { FC } from 'react';
-import event from "../../assets/Layer_1.png";
-import left from "../../assets/angle-small-left (1).png";
-import rigth from "../../assets/angle-small-right.png";
+import { FC, useRef, useState } from 'react';
 import './hero.css';
-import { Item } from './Item';
+import { SlideItem } from './slideItem';
+
+// Icons
+import home from "../../assets/event.png";
+import event from "../../assets/event.png";
+import glory from "../../assets/glory.png";
+import contact from "../../assets/contact.png";
+import team from "../../assets/team.png";
+import left from "../../assets/angle-small-left.png";
+import rigth from "../../assets/angle-small-right.png";
 
 const Hero: FC = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const slideRef = useRef<HTMLDivElement | null>(null);
 
-    const enum BgColor {
-        red = 'bg-custom-red',
-        green = 'bg-custom-green',
-        blue = 'bg-custom-blue',
-        yellow = 'bg-custom-yellow',
-    }
+    const handleNext = () => {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % listSlideItems.length);
+        if (slideRef.current) {
+            const firstItem = slideRef.current.children[0];
+            slideRef.current.appendChild(firstItem);
+        }
+    };
 
-    const listItems = [
+    const handlePrev = () => {
+        setActiveIndex((prevIndex) => (prevIndex - 1 + listSlideItems.length) % listSlideItems.length);
+        if (slideRef.current) {
+            const lastItem = slideRef.current.children[slideRef.current.children.length - 1];
+            slideRef.current.prepend(lastItem);
+        }
+    };
+
+    const listSlideItems = [
         {
-            logo: event,
-            title: "Event",
+            logo: home,
+            title: "Home",
             description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
-            bgColor: BgColor.red,
+            color: "-custom-red",
             button: {
                 label: "See More",
                 href: "/event"
             }
+        },
+        {
+            logo: event,
+            title: "Event",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+            color: "-custom-red",
+            button: {
+                label: "See More",
+                href: "/event"
+            }
+        },
+        {
+            logo: glory,
+            title: "Glory",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+            color: "-custom-blue",
+            button: {
+                label: "See More",
+                href: "/glory"
+            }
+        },
+        {
+            logo: team,
+            title: "Team",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+            color: "-custom-yellow",
+            button: {
+                label: "See More",
+                href: "/team"
+            }
+        },
+        {
+            logo: contact,
+            title: "Contact",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+            color: "-custom-green",
+            button: {
+                label: "See More",
+                href: "/contact"
+            }
         }
     ];
 
+    const handleItemClick = (index: number) => {
+        if (!(index === activeIndex)) {
+            setActiveIndex(index);
+            if (slideRef.current) {
+                const firstItem = slideRef.current.children[0];
+                slideRef.current.appendChild(firstItem);
+            }
+        }
+    }
     return (
         <section className="flex flex-col h-full w-screen md:h-screen">
             <div className="overflow-hidden w-full relative h-screen flex justify-center">
-                <div className="slide">
-                    {/* Main Item */}
-                    <div className="item">
-                        <div className="w-full rounded-[20px] flex justify-evenly items-center gap-6 flex-col bg-red-500 backdrop-filter backdrop-blur-lg bg-opacity-30 ref">
-                            <img src={event} alt="icon" className="w-24" />
-                            <h1 className="text-4xl text-gray-400">Events</h1>
-                        </div>
-
-                        <div className="content">
-                            <div className="name text-5xl font-bold my-3">
-                                <span className="text-custom-red">H</span>
-                                <span className="text-custom-blue">o</span>
-                                <span className="text-custom-green">m</span>
-                                <span className="text-custom-yellow">e</span>
-                            </div>
-                            <div className="des text-lg text-white">
-                                The most secure marketplace for buying and selling unique crypto assets.
-                            </div>
-                            <a
-                                href="#"
-                                className="flex justify-center items-center w-auto bg-white text-gray-900 font-semibold py-2 px-6 mb-3 rounded-full hover:bg-gray-200"
-                            >
-                                See MORE
-                            </a>
-                        </div>
-                    </div>
-
-                    {/* List Items */}
-                    {listItems.map((item, index) => (
-                        <Item
+                <div ref={slideRef} className="slide ">
+                    {listSlideItems.map((item, index) => (
+                        <SlideItem
                             key={index}
+                            index={index}
+                            activeIndex={activeIndex}
                             image={item.logo}
                             title={item.title}
                             description={item.description}
                             button={item.button}
-                            bgColor={item.bgColor}
+                            color={item.color}
+                            onClick={() => handleItemClick(index)}
                         />
                     ))}
                 </div>
 
                 <div className="gap-5 absolute bottom-4 flex justify-center items-center md:hidden py-2">
-                    <span className="prev btn">
-                        <img src={left} alt="" />
-                    </span>
-                    <span className="next btn">
-                        <img src={rigth} alt="" />
-                    </span>
+                    <button onClick={handlePrev} className="prev btn">
+                        <img src={left} alt="Previous" />
+                    </button>
+                    <button onClick={handleNext} className="next btn">
+                        <img src={rigth} alt="Next" />
+                    </button>
                 </div>
             </div>
 
             {/* Cards */}
-            <div className="relative flex justify-center items-center md:items-end flex-col sm:flex-row gap-6 px-6">
+            <div className="relative flex justify-center items-center md:items-end flex-col sm:flex-row gap-6 px-6 ">
                 {/* Card Item */}
-                <div className="bg-custom-green h-32 flex justify-evenly md:justify-start items-center flex-col py-3 text-2xl p-6">
-                    <h1>200</h1>
-                    <h1>Number</h1>
-                </div>
-                {/* Card Item */}
-                <div className="bg-custom-blue h-32 flex justify-evenly md:justify-start items-center flex-col py-3 text-2xl p-6">
-                    <h1>200</h1>
-                    <h1>Number</h1>
-                </div>
-                {/* Card Item */}
-                <div className="bg-custom-red h-32 flex justify-evenly md:justify-start items-center flex-col py-3 text-2xl p-6">
-                    <h1>200</h1>
-                    <h1>Number</h1>
+                <div className="rounded-[20px] sm:rounded-[0] sm:rounded-t-[20px]  md:rounded-t-[20px] w-auto h-40 px-6 py-0 text-2xl flex flex-col justify-evenly items-center sm:gap-6 sm:py-5 md:justify-start bg-custom-green backdrop-filter backdrop-blur-lg bg-opacity-30 ">
+                    <span>200</span>
+                    <span>Numbers</span>
                 </div>
             </div>
         </section>
