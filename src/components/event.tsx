adfb85a90868d480196de5c6936c7415996241d7
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2025-01-21 14:25:09 +0100

    [add] create event section

---
+++ src/components/event.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+const Event: FC = () => {
+
+    return (
+        <section className="relative h-full w-screen overflox-x-hidden flex justify-center items-center flex-col my-10  gap-6 py-5">
+            <h1 className="text-5xl text-center ">
+                Google GameFest
+            </h1>
+            <div className="text-white w-3/4 text-wrap bg-gray-600 backdrop-blur-lg bg-opacity-40 p-6 rounded-[20px] my-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aspernatur ad commodi dicta illo? Dignissimos eum iure laborum repellat officiis, ut ipsum dolor voluptate tenetur temporibus quas, ad corporis id?
+                Fugit illum nulla in commodi reiciendis, vel optio molestias facere. Quia, rem explicabo veniam aperiam molestias et laudantium cumque non obcaecati ut facere debitis voluptatem sit ad corrupti quos? Fugit!
+                Placeat ab voluptates officiis possimus aut architecto rem nam voluptate debitis perferendis, ut aliquid iusto facilis quis omnis earum impedit vero deserunt est, porro magnam, molestiae exercitationem. Fugit, officia molestias!
+                Architecto adipisci eaque, reprehenderit quisquam quo itaque eligendi nihil consectetur in porro suscipit nobis unde dolore ea optio quae dolorem facere! Voluptates esse sunt facilis dicta autem, non enim provident!
+                Iste id labore nemo, culpa harum maiores dolores optio in corporis, facere sequi ab ea quidem beatae cupiditate quasi. Molestiae, doloribus! Assumenda pariatur laborum dolorum quaerat aspernatur rerum aperiam ullam.
+            </div>
+            <a href="#more" className={`flex justify-center items-center w-auto  font-semibold py-3 px-12 mb-3 rounded-3xl border `}>
+                EXPLORE MORE
+            </a>
+            <div>
+                .....
+            </div>
+        </section >
+    );
+};
+
+export default Event;
